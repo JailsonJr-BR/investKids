8171e4bdf1bb04c0710ae1472378f41cfa37b7a3
Jailson JR <jailsonjunior513@gmail.com>
2022-04-28 15:04:50 -0300

    só deus sabe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//menu hamburguer
+
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector("._nav-menu");
 
@@ -18,3 +20,18 @@ const overlay = document.querySelector("._overlay")
 window.addEventListener("load", function(){
   overlay.style.display = "none";
 })
+
+
+
+const language = document.querySelector("._header-language");
+const languageh = document.querySelector("._header-languageh");
+
+language.addEventListener("click", () => {
+  language.classList.toggle("active");
+  languageh.classList.toggle("active");
+})
+
+document.querySelectorAll("._nav-link").forEach(n => n.addEventListener("click", () => {
+  language.classList.remove("active");
+  languageh.classList.remove("active");
+}))
