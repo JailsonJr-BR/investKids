8ab6ebc69152a1cb2c98c8fc6578cb1608e470f4
Jailson JR <jailsonjunior513@gmail.com>
2022-04-30 06:09:34 -0300

    teste language

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ window.addEventListener("load", function(){
 })
 
 
-
+//language menu
 const language = document.querySelector("._header-language");
 const languageh = document.querySelector("._header-languageh");
 
@@ -35,3 +35,32 @@ document.querySelectorAll("._nav-link").forEach(n => n.addEventListener("click",
   language.classList.remove("active");
   languageh.classList.remove("active");
 }))
+
+//teste
+var dataReload = document.querySelectorAll("[data-reload]");
+
+var lang = {
+  en: {
+    teste1: "Free trial"
+  },
+  es: {
+    teste1: "Test pichula",
+    homep: "Metod Revoluc"
+  },
+  pt: {
+    teste1: "Teste gratis"
+  }
+};
+
+if (window.location.hash) {
+  if (window.location.hash === "#es") {
+    teste.textContent = lang.es.teste1;
+    homep.textContent = lang.es.homep;
+  }
+}
+
+for (i = 0; i <= dataReload.length; i++) {
+  dataReload[i].onclick = function() {
+    location.reload(true);
+  };
+}
